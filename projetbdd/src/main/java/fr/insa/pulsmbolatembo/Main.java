5ca841a469e77234720a9584486648ca56e1dcd7
Regis Mbola Tembo <regis.mbola_tembo@insa-strasbourg.fr>
2023-10-09 14:20:11 +0200

    09_10_2023

--- projetbdd/src/main/java/fr/insa/pulsmbolatembo/Main.java
+++ projetbdd/src/main/java/fr/insa/pulsmbolatembo/Main.java
@@ -1,6 +1,7 @@
 package fr.insa.pulsmbolatembo;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class Main {
 
@@ -14,10 +15,23 @@ public class Main {
         return this.con;
     }
 
-    public void Connect(Connection c){
+    public void connect(Connection c){
         this.con=c;
     }
     
+    public Connection setupCon(String url, String username, String mdp){
+        Connection c;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection(url,username,mdp);
+            System.out.println("Connexion établie");
+        }
+        catch (Exception e){
+            System.out.println("echec de connexion :"+e);
+        }
+        return c;
+    }
+
     public static void debut(){
         try (Connection con = connectSurServeurM3()){
             System.out.println("connecté");
@@ -25,6 +39,11 @@ public class Main {
             gestionnaire.
         }
     }
+
     public static void main(String[] args) {
+    }
+
+    public static void interfaceTextuelle(){
+
     }
 }
