16e15413edf4c8a0bb5f24a2a7c73023f2e14282
Violette <violette.puls@gmail.com>
2023-11-09 11:33:19 +0100

    NavBar

---
+++ projetbdd/src/main/java/com/example/application/views/main/AppLayoutNavbarPlacement.java
@@ -0,0 +1,58 @@
+package com.example.application.views.main;
+
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouterLink;
+
+@Route("app-layout-navbar-placement")
+public class AppLayoutNavbarPlacement extends AppLayout {
+
+    public AppLayoutNavbarPlacement() {
+        DrawerToggle toggle = new DrawerToggle();
+
+        H1 title = new H1("Atelier de fabrication");
+        title.getStyle().set("font-size", "var(--lumo-font-size-l)")
+                .set("margin", "0");
+
+        Tabs tabs = getTabs();
+
+        addToDrawer(tabs);
+        addToNavbar(toggle, title);
+    }
+
+    private Tabs getTabs() {
+        Tabs tabs = new Tabs();
+        tabs.add(createTab(VaadinIcon.CART, "Production"),
+                createTab(VaadinIcon.CALENDAR, "Emploi du temps"),
+                createTab(VaadinIcon.PACKAGE, "Produits"),
+                createTab(VaadinIcon.USERS, "Opérateur"),
+                createTab(VaadinIcon.WRENCH, "Machines"),
+                createTab(VaadinIcon.LIST, "Gestion Atelier"));
+        tabs.setOrientation(Tabs.Orientation.VERTICAL);
+        return tabs;
+    }
+
+    private Tab createTab(VaadinIcon viewIcon, String viewName) {
+        Icon icon = viewIcon.create();
+        icon.getStyle().set("box-sizing", "border-box")
+                .set("margin-inline-end", "var(--lumo-space-m)")
+                .set("margin-inline-start", "var(--lumo-space-xs)")
+                .set("padding", "var(--lumo-space-xs)");
+
+        RouterLink link = new RouterLink();
+        link.add(icon, new Span(viewName));
+        // Demo has no routes
+        // link.setRoute(viewClass.java);
+        link.setTabIndex(-1);
+
+        return new Tab(link);
+    }
+
+}
--- projetbdd/src/main/java/com/example/application/views/main/MainView.java
+++ projetbdd/src/main/java/com/example/application/views/main/MainView.java
@@ -1,16 +1,23 @@
 package com.example.application.views.main;
 
+import java.util.ArrayList;
+import java.util.List;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
 @PageTitle("Main")
 @Route(value = "")
-public class MainView extends HorizontalLayout {
+public class MainView extends VerticalLayout {
 
     private TextField name;
     private Button sayHello;
@@ -24,9 +31,17 @@ public class MainView extends HorizontalLayout {
         sayHello.addClickShortcut(Key.ENTER);
 
         setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+        setHorizontalComponentAlignment(Alignment.CENTER, name, sayHello);
 
-        add(name, sayHello);
+        //add(name, sayHello);
+        //TEST VAADIN BOUTON+CALENDRIER
+        AppLayoutNavbarPlacement navBar=new AppLayoutNavbarPlacement();
+
+        add(navBar,name, sayHello);
+
     }
 
+
+
 }
+
